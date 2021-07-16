0b73517f18e4729eaa7342657163f53f522df552
Mertt <Mertt@DESKTOP-LHRACU7>
2021-07-16 13:16:18 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elmasi";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            //c# type-safe'dir
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------------");
+            }
+
+            Console.WriteLine("--------------Methods-------------");
+            // Instance 
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yesil elma", 12, 9);
+            sepetManager.Ekle2("karpuz", "Diyarbakir karpuzu", 12, 8);
+
+        }
+    }
+}
+
+
+
+// Do not repeat yourself - DRY - Clean Code 
