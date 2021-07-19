10eacbc9823ec1bf5f4e03069743bb26e4a1b9c3
Mert Bulut <MerttBulutt@outlook.com>
2021-07-19 14:23:32 +0300

    19/07

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    //MicroServise
+    class GamerManager : IGamerService
+    {
+        IUserValidationService userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            this.userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayit oldu");
+            }
+            else
+            {
+                Console.WriteLine("Dogrulama basarisiz. Kayit basarisiz.");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayit silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayit guncellendi");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1985, FirstName = "Engin", LastName = "Demirog", IdentityNumber = 12345 });
+        }
+    }
+}
